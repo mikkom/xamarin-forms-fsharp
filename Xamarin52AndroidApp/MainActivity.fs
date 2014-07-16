300d50a40b10045e76f50d28d1e55c4d31dc7f11
Mikko Matilainen <mikkom@iki.fi>
2014-07-16 14:45:17 +0300

    Initial commit.

---
+++ Xamarin52AndroidApp/MainActivity.fs
@@ -0,0 +1,30 @@
+﻿namespace Xamarin52AndroidApp
+
+open System
+
+open Android.App
+open Android.Content
+open Android.OS
+open Android.Runtime
+open Android.Views
+
+open Xamarin.Forms
+open Xamarin.Forms.Platform.Android
+
+type App () =
+    static member GetMainPage () =
+        new NavigationPage ()
+
+[<Activity (Label = "Xamarin52AndroidApp", MainLauncher = true)>]
+type MainActivity () =
+    inherit AndroidActivity ()
+
+    let mutable count:int = 1
+
+    override this.OnCreate (bundle) =
+
+        base.OnCreate (bundle)
+
+        Forms.Init (this, bundle)
+
+        this.SetPage <| App.GetMainPage ()
